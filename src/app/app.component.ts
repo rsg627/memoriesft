9aae93d72f5cdb62948bcc402e5462df2c59ef47
Rodol SG <rsg627@gmail.com>
2024-11-21 23:04:38 -0600

    improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterModule, RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatCardModule } from '@angular/material/card';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,MatIconModule,MatButtonModule,MatToolbarModule,RouterModule],
+  imports: [RouterOutlet,MatIconModule,MatButtonModule,MatToolbarModule,RouterModule,MatCardModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  constructor(private router:Router){}
+
   title = 'memoriesft';
+  getRoute(){
+    console.log(this.router.url=='/');
+    return this.router.url=='/';
+  }
 }
